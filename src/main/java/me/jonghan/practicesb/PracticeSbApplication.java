4d77e9faaa6e995be686cdb95c2954dec5f380e9
jong-k <fully.charged07@gmail.com>
2025-04-27 18:13:27 +0900

    init project

---
+++ src/main/java/me/jonghan/practicesb/PracticeSbApplication.java
@@ -0,0 +1,12 @@
+package me.jonghan.practicesb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSbApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(PracticeSbApplication.class, args);
+  }
+
+}
