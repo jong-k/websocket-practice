413f6f7ebd5636955aab3fc7de01a4111063b5c3
jong-k <fully.charged07@gmail.com>
2025-08-03 16:42:24 +0900

    feat: card header ui 완성

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -7,16 +7,23 @@ import {
   CardContent,
   CardFooter,
 } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { RefreshCcw } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center">
-      <div className="w-xl h-3xl">
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="w-xl">
         <Card>
           <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-            <CardAction>Card Action</CardAction>
+            <CardTitle>Simple Chat</CardTitle>
+            <CardDescription>지금 실시간으로 연결되어 보세요</CardDescription>
+            <CardAction>
+              <Button className="cursor-pointer" variant="outline" size="sm">
+                <RefreshCcw />
+                초기화
+              </Button>
+            </CardAction>
           </CardHeader>
           <CardContent>
             <p>Card Content</p>
